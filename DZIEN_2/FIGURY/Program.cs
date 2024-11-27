46a0d6853b9d7c44a5b251e55a904eec6c8a6ba3
albim72 <marcin.albiniak72@gmail.com>
2024-11-27 15:14:39 +0100

    Update Program.cs

--- DZIEN_2/FIGURY/Program.cs
+++ DZIEN_2/FIGURY/Program.cs
@@ -17,7 +17,11 @@ namespace Figury
             Trojkat tr = new Trojkat(6.5, 7.8);
             Console.WriteLine($"pole trójkąta: {tr.policzPole()}");
 
+            Trapez trp = new Trapez(7.4,5.4,4.3);
+            Console.WriteLine($"pole trapeza: {trp.policzPole()}");
 
+            Kolo kl = new Kolo(5.5);
+            Console.WriteLine($"pole koła: {kl.policzPole()}");
             Console.ReadKey();
         }
     }
